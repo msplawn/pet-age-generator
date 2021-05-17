662361c7abbc1678e2d489a77c7ce3906e2123de
msplawn <mmsplawn@gmail.com>
2021-05-17 15:53:10 -0500

    Created initial files and started script

---
+++ script.js
@@ -0,0 +1,12 @@
+const genBtn = document.getElementById("generate-button");
+const ageEl = document.getElementById("age");
+
+const init = () => {
+    const inputs = inputs();
+}
+
+const inputs = () => {
+    
+}
+
+ageEl.addEventListener("click", init);
